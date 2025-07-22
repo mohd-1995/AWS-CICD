a1e648d442324f60b61c60df547ac9fad7cd8e2f
mohd_liverpool1@hotmail.co.uk <mohd_liverpool1@hotmail.co.uk>
2025-07-22 04:06:05 +0100

    test1

--- infra/tools.sh
+++ infra/tools.sh
@@ -46,9 +46,6 @@ kubectl version --client
 aws eks update-kubeconfig --region eu-west-2 --name my-cluster
 
 
-
-
-
 # Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
